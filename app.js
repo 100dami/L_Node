bccb33b76686896b98f4c281cc3ae3f564ff476f
dami <100damii@naver.com>
2021-04-13 21:15:36 +0900

    ADD code for use nunjucks

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const admin = require('./Routes/admin');
 const contacts = require('./Routes/contacts');
 
 const app = express();
 const port = 3000;
+
+nunjucks.configure('template', {
+    autoescape : true,
+    express : app
+})
 
 app.get('/', (req, res) => {
     res.send('hello express');
