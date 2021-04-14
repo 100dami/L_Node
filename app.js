8da66d05950ff90d7d174a90e515d5a209714912
dami <100damii@naver.com>
2021-04-14 23:37:00 +0900

    ADD code for setting middleware, MAKE funcion

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const logger = require('morgan');
 
 const admin = require('./Routes/admin');
 const contacts = require('./Routes/contacts');
@@ -8,14 +9,23 @@ const app = express();
 const port = 3000;
 
 nunjucks.configure('template', {
-    autoescape : true,
+    autoescape : true, //html 태그 사용을 막음
     express : app
-})
+});
+
+// 미들웨어 세팅
+app.use(logger('dev'));
+
 
 app.get('/', (req, res) => {
     res.send('hello express');
 });
 
+function vipMiddleware(req, res, next) {
+    console.log('최우선 미들웨어');
+    next();
+}
+// app.use('/admin', vipMiddleware, admin);
 app.use('/admin', admin);
 app.use('/contacts', contacts);
 
