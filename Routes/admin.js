2765f0c1e5e1e678be6c07825c490dbcc2dcbfa8
dami <100damii@naver.com>
2021-04-14 23:34:12 +0900

    ADD html code, connect base.html

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+function testMiddleware( req, res, next) {
+    console.log('첫번째 미들웨어');
+    next();
+}
 
-router.get('/', (req, res) => {
+function testMiddleware2( req, res, next) {
+    console.log('두번째 미들웨어');
+    next();
+}
+
+router.get('/', testMiddleware, testMiddleware2, (req, res) => {
     res.send('admin 이후 url');
 });
 
 router.get('/products', (req, res) => {
     res.render('admin/products.html', {
-        online : 'BBoooooㅊㅊㅊㅊㅊoooo'
+        online : '<h1>태그가 출력됩니다 </h1>'
     })
 });
 
