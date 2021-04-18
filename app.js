bec1e6c77cbf1ca643bc959a0cbe911acc21d6f3
dami <100damii@naver.com>
2021-04-18 17:25:20 +0900

    isLogin 변수 만들기

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const admin = require('./Routes/admin');
 const contacts = require('./Routes/contacts');
@@ -15,8 +16,16 @@ nunjucks.configure('template', {
 
 // 미들웨어 세팅
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
+app.use('/uploads', express.static('uploads')); //앞에는 url 명 설정, 뒤에는 파일경로 설정
+// 사용 방법 : http://localhost:3000/uploads/bear.png
 
+app.use( (req, res, next) => {
+    app.locals.isLogin = false;
+    next();
+})
 app.get('/', (req, res) => {
     res.send('hello express');
 });
